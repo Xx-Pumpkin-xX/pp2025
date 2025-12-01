d1729428c3b551d20896a9a705eff95d2f3a3c14
Xx-Pumpkin-xX <luckynoob410@gmail.com>
2025-12-01 10:21:55 +0700

    []

--- python/2.student.mark.oop.py
+++ python/2.student.mark.oop.py
@@ -2,59 +2,76 @@ student = []
 course = []
 mark = {}
 
-def set_number_of_student():
-    count = int(input("Enter the number of student in the class: "))
-    return count
-def set_student_info():
-    count = set_number_of_student()
+class BasicInfo():
+    def __init__(self, n, id):
+        self.name = n
+        self.id = id
+    def describe(self):
+        return f"ID: {self.id}  |  Name: {self.name}"
+
+class Student(BasicInfo):
+    def __init__(self, n, id, dob):
+        super().__init__(n, id)
+        self.dob = dob
+    
+class Course(BasicInfo):
+    def __init__(self, n, id):
+        super().__init__(n, id)
+
+def set_student():
+    count = int(input("Enter number of students: "))
     for i in range (count):
         print(f"---Student {i+1}---")
         s_name = str(input(f"Name: "))
         s_id = str(input(f"ID: "))
-        DoB = str(input(f"DoB: "))
-        student.append({"name" : s_name, "id" : s_id, "DoB" : DoB})
-def set_number_of_course():
-    count = int(input("Enter the number of course: "))
-    return count
-def set_course_info():
-    count = set_number_of_course()
+        s_dob = str(input(f"DoB: "))
+        new_s = Student(s_name, s_id, s_dob)
+        student.append(new_s)
+    
+def set_course():
+    count = int(input("Enter number of courses: "))
     for i in range (count):
         print(f"---Course {i+1}---")
         c_name = str(input(f"Name: "))
         c_id = str(input(f"ID: "))
-        course.append({"name" : c_name, "id" : c_id})
+        new_c = Course(c_name, c_id)
+        course.append(new_c)
         mark[c_id] = {}
+     
 def set_mark():
     if not course:
         print("No course avaiable")
         return
     for c in course:
-        print(f"Available courses: {c['name']} (ID: {c['id']})")
+        print(c.describe())
     selected_id = input("Enter course ID to get mark: ")
     if selected_id in mark:
         for s in student:
-            score = float(input(f"Enter mark for {s['name']} (ID {s['id']}): "))
-            mark[selected_id][s['id']] = score
+            score = float(input(f"Enter mark for {s.name} (ID {s.id}): "))
+            mark[selected_id][s.id] = score
     else:
         print("Invalid Course ID")
+        
 def get_student():
     print("")
     print("-----Student list-----")
     for s in student:
-        print(f"ID: {s['id']}, Name: {s['name']}, DoB: {s['DoB']}")
+        print(f"ID: {s.id}, Name: {s.name}, DoB: {s.dob}")
+        
 def get_mark():
     print("")
     print("-----Showing Mark-----")
     for c in course:
-        print(f"Available courses: {c['name']} (ID: {c['id']})")
+        print(f"Available courses: {c.name} (ID: {c.id})")
     c_id = input("Enter the Course ID: ")
     if c_id in mark:
-        print(f"Mark for {c['name']}: ")
+        print(f"Mark for {c.name}: ")
         for s in student:
-            score = mark[c_id].get(s['id'], "N/A")
-            print(f"{s['name']}: {score}")
+            score = mark[c_id].get(s.id, "N/A")
+            print(f"{s.name}: {score}")
     else:
         print("Course not found")
+
 def main():
     while True:
         print("\n1. Input Students")
@@ -68,10 +85,10 @@ def main():
         
         if user == '1':
             print("-----Setting student info: -----")
-            set_student_info()
+            set_student()
         elif user == '2':
             print("-----Setting course info: ------")
-            set_course_info()
+            set_course()
         elif user == '3':
             print("-----Setting mark for student: -----")
             set_mark()
